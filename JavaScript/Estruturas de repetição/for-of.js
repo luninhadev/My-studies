952b56bd01e3cba434e926e6920c89c4e1e51d8e
Luna de Almeida <luna.almeidaa@outlook.com>
2022-12-03 17:50:46 -0300

    atualizando

---
+++ JavaScript/Estruturas de repetição/for-of.js
@@ -0,0 +1,14 @@
+// for...of
+
+let name = 'Luna'
+let names = ['João', 'Paulo', 'Pedro']
+
+// mostra todas as letras do nome
+for(let char of name){
+    console.log(char)
+}
+
+// mostra todas os nomes do array
+for(let name of names){
+    console.log(name)
+}
--- JavaScript/Estruturas de repetição/for.js
+++ JavaScript/Estruturas de repetição/for.js
@@ -1,2 +1,11 @@
 // Estruturas de repetição
 // for
+// break - para a execução do loop
+// continue - pula a execução do momento
+
+for (let i = 10; i > 0; i--){
+    if(i === 5){
+        continue;
+    }
+    console.log(i)
+}
